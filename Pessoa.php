74dee342a69d05e962f32413bbb6a9080c27adbc
Juscelino Fernandes da Costa Junior <juscostajr@gmail.com>
2025-07-29 22:45:19 -0300

    encapsulando a classe Pessoa

--- Pessoa.php
+++ Pessoa.php
@@ -2,16 +2,28 @@
 
 class Pessoa
 {
-    public float $massa;
-    public float $altura;
+    private float $massa;
+    private float $altura;
+
+    public function __construct(float $massa, float $altura)
+    {
+        $this->massa = $massa;
+        $this->altura = $altura;
+    }
 
     public function calcularIMC(): float
     {
         return $this->massa / ($this->altura ** 2);
     }
+
+    public function getMassa(): float
+    {
+        return $this->massa;
+    }
 }
 
-$pessoa = new Pessoa();
-$pessoa->massa = 80;
-$pessoa->altura = 1.8;
+
+$pessoa = new Pessoa(massa: 70, altura: 1.7);
+
+echo "A massa da pessa é {$pessoa->getMassa()}\n";
 echo "O IMC da pessoa é {$pessoa->calcularIMC()}\n";
